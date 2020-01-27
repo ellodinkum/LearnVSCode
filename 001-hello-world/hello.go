7e9e23aebf7843730d05206c634a74b7a7522d94
ellodinkum <faisal375@hotmail.com>
2020-01-28 10:33:18 +1100

    testing the commits directly to github

--- 001-hello-world/hello.go
+++ 001-hello-world/hello.go
@@ -1,5 +1,18 @@
 package main
 
+import "fmt"
+
 func main() {
 	fmt.Println("hello world")
+
+	fmt.Print(a)	
+	fmt.Print(b)
+	fmt.Print(c)
+	fmt.Print(c)
+	fmt.Print(c)
+	fmt.Print(c)
+	fmt.Print(c)
+	fmt.Print(d)
 }
+
+
